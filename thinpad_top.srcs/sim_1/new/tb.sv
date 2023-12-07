370662f49b72867837ecb9f6ee9de7e11cfa77c2
btree <juanli-w21@mails.tsinghua.edu.cn>
2023-12-08 06:12:41 +0800

    test

--- thinpad_top.srcs/sim_1/new/tb.sv
+++ thinpad_top.srcs/sim_1/new/tb.sv
@@ -47,7 +47,7 @@ module tb;
 
   // Windows �?要注意路径分隔符的转义，例如 "D:\\foo\\bar.bin"
   `define DWN "D:\\rv-2023\\asmcode\\lab6.bin"
-  `define WJL "D:\\Codefield\\ComputerOrganization\\rv-2023\\asmcode\\kernel-rv32-no16550.bin"
+  `define WJL "D:\\Codefield\\ComputerOrganization\\rv-2023\\asmcode\\test.bin"
   // parameter BASE_RAM_INIT_FILE = "D:\\Codefield\\ComputerOrganization\\rv-2023\\asmcode\\test.bin"; // BaseRAM 初始化文件，请修改为实际的绝对路�?
   // parameter BASE_RAM_INIT_FILE = "D:\\code\\cod23-grp53\\rvtests_simple\\test19.bin";
   parameter BASE_RAM_INIT_FILE = `WJL; // dwn
